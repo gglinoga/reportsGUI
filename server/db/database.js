b7bdb526965f41c64027e1af9ed098e66c797d58
Greg Glinoga <gglinoga@gmail.com>
2019-07-05 19:12:20 -0700

    n

--- server/db/database.js
+++ server/db/database.js
@@ -13,21 +13,21 @@ console.log('knex connected');
 
 
 dropTables = () => {
-    knex.schema.hasTable('FBICrimeState').then(
-        knex.schema.dropTable('FBICrimeState').then(() => {
-            console.log('dropped FBICrimeState');
-        })
-    );
-    knex.schema.hasTable('FBICrimeArea').then(
-        knex.schema.dropTable('FBICrimeArea').then(() => {
-            console.log('dropped FBICrimeArea')
-        })
-    );
-    knex.schema.hasTable('FBICrimeRegion').then(
-        knex.schema.dropTable('FBICrimeRegion').then(() => {
-            console.log('dropped FBICrimeRegion');
-        })
-    );
+    // knex.schema.hasTable('FBICrimeState').then(
+    //     knex.schema.dropTable('FBICrimeState').then(() => {
+    //         console.log('dropped FBICrimeState');
+    //     })
+    // );
+    // knex.schema.hasTable('FBICrimeArea').then(
+    //     knex.schema.dropTable('FBICrimeArea').then(() => {
+    //         console.log('dropped FBICrimeArea')
+    //     })
+    // );
+    // knex.schema.hasTable('FBICrimeRegion').then(
+    //     knex.schema.dropTable('FBICrimeRegion').then(() => {
+    //         console.log('dropped FBICrimeRegion');
+    //     })
+    // );
 }
 
 createRegion = () => {
@@ -58,6 +58,18 @@ createState = () => {
         table.string('state');
         table.integer('areaID').unsigned();
         table.foreign('areaID').references('FBICrimeArea.areaID');
+    }).then((response, err) => {
+        if (err) throw err;
+        createFBICrimeData();
+    });
+}
+
+createFBICrimeData = () => {
+    knex.schema.createTable('FBICrimeData', table => {
+        table.increments('dataID');
+        table.integer('stateID').unsigned();
+        table.foreign('stateID').references('FBICrimeState.stateID');
+        table.integer('year');
         table.integer('population');
         table.integer('violentCrime');
         table.integer('murderManSlaughter');
@@ -70,19 +82,20 @@ createState = () => {
         table.integer('gta');
     }).then((response, err) => {
         if (err) throw err;
-    });
+    })
 }
 
-let start = (err) => {
-    // dropTables();
-    if (err) {
-        console.log(err)
-    } else {
-        createRegion();
-    }
-}
+// let start = (err) => {
+//     // dropTables();
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         createRegion();
+//     }
+// }
 
 // start();
+// createFBICrimeData();
 
 
 module.exports = knex;
